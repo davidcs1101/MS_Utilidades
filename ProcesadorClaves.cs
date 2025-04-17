09978b273593b899f24ef891997d5b98ab1aee96
Edison Catano <edisongim@hotmail.com>
2025-04-17 12:57:15 -0500

    PBIXXXXX _ Se incorpora el procesador de claves para que sea una utilidad que podamos usar desde cualquier proyecto de microservicio.

---
+++ ProcesadorClaves.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Utilidades
+{
+    public static class ProcesadorClaves
+    {
+        //Para generar una clave segura
+        public static string GenerarClaveSegura(int longitud) 
+        {
+            byte[] bytesAleatorios = new byte[longitud];
+            using (var cripto = RandomNumberGenerator.Create())
+            {
+                cripto.GetBytes(bytesAleatorios);
+            }
+            return Convert.ToBase64String(bytesAleatorios);
+        }
+
+        public static string EncriptarClave(string clave) 
+        {
+            using(var sha256 = SHA256.Create()) 
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(clave));
+                return Convert.ToBase64String(bytes);
+            }
+        }
+    }
+}
