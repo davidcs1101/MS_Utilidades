38cb8b3ee8aa0634172aaa95ac2b0e63cfa4de15
EDISON <edisongim@hotmail.com>
2025-11-22 14:03:16 -0500

    20251122 _ Adicion de propiedad.

--- CodigosListas.cs
+++ CodigosListas.cs
@@ -3,5 +3,6 @@
     public static class CodigosListas
     {
         public const string TIPOSIDENTIFICACION = "TIPOSIDENTIFICACION";
+        public const string NIVELESCOMPLEJIDAD = "NIVELESCOMPLEJIDAD";
     }
 }
