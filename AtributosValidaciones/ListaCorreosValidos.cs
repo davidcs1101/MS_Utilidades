f288744b387f8e9b2bb036af3848cb61c0d4d3c6
ZEUS\ediso <edisongim@hotmail.com>
2026-05-30 18:49:55 -0500

    PBIXXXX _ Se agrega dos atributos de validaciones, uno para listas y verificar que contenga elementos, otro para formatos de correo.

---
+++ AtributosValidaciones/ListaCorreosValidos.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Utilidades.AtributosValidaciones
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class ListaCorreosValidos : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? valor,ValidationContext validationContext)
+        {
+            if (valor == null)
+                return ValidationResult.Success;
+
+            if (valor is not IEnumerable<string> correos)
+                return new ValidationResult("El atributo solo puede aplicarse a listas de correos.");
+
+            var validadorCorreo = new EmailAddressAttribute();
+            foreach (var correo in correos)
+            {
+                if (string.IsNullOrWhiteSpace(correo))
+                    return new ValidationResult("No se permiten correos vacíos.");
+
+                if (!validadorCorreo.IsValid(correo))
+                    return new ValidationResult(
+                        $"{ErrorMessage ?? Textos.Generales.VALIDA_CORREO_NO_VALIDO}: {correo}");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ AtributosValidaciones/ListaDebeContenerElementos.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections;
+
+namespace Utilidades.AtributosValidaciones
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class ListaDebeContenerElementos : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(
+            object? valor,
+            ValidationContext validationContext)
+        {
+            if (valor == null)
+                return new ValidationResult(ErrorMessage ?? $"{validationContext.DisplayName} es obligatorio.");
+
+            if (valor is not IEnumerable lista)
+                return new ValidationResult("El atributo solo puede aplicarse a colecciones.");
+
+            var enumerador = lista.GetEnumerator();
+
+            if (!enumerador.MoveNext())
+            {
+                return new ValidationResult(ErrorMessage ??
+                    $"{validationContext.DisplayName} debe contener al menos un elemento.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
