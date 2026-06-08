f5705701aaaa7ea17a580102650bc52dfb2b5f1b
ZEUS\ediso <edisongim@hotmail.com>
2026-06-08 00:32:59 -0500

    PB _ Se crea clase para tipos Mime.

---
+++ TiposMime.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilidades
+{
+    public static class TiposMime
+    {
+        public static string ObtenerTipoMime(string extension)
+        {
+            if (string.IsNullOrWhiteSpace(extension))
+                return "application/octet-stream";
+
+            extension = extension.Trim().ToLowerInvariant();
+
+            return extension switch
+            {
+                ".pdf" => "application/pdf",
+
+                ".txt" => "text/plain",
+                ".csv" => "text/csv",
+
+                ".doc" => "application/msword",
+                ".docx" => "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+
+                ".xls" => "application/vnd.ms-excel",
+                ".xlsx" => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+
+                ".ppt" => "application/vnd.ms-powerpoint",
+                ".pptx" => "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+
+                ".jpg" => "image/jpeg",
+                ".jpeg" => "image/jpeg",
+                ".png" => "image/png",
+                ".gif" => "image/gif",
+                ".bmp" => "image/bmp",
+                ".webp" => "image/webp",
+
+                ".zip" => "application/zip",
+                ".rar" => "application/vnd.rar",
+                ".7z" => "application/x-7z-compressed",
+
+                ".xml" => "application/xml",
+                ".json" => "application/json",
+
+                _ => "application/octet-stream"
+            };
+        }
+    }
+}
