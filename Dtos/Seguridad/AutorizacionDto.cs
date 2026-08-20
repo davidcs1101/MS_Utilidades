164cf6a4fa85405935f5d635d82268ca9933f9a6
ZEUS\ediso <edisongim@hotmail.com>
2026-08-19 19:53:05 -0500

    no message

---
+++ Dtos/Seguridad/AutorizacionDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Utilidades.Dtos.Seguridad
+{
+    public class AutorizacionDto
+    {
+        public int Id { get; set; }
+        public string CodigoPrograma { get; set; } = null!;
+        public bool EstadoPrograma { get; set; }
+        public string CodigoGrupo { get; set; } = null!;
+        public bool EstadoGrupo { get; set; }
+        public string CodigoPermiso { get; set; } = null!;
+        public bool EstadoPermiso { get; set; }
+        public bool EstadoGrupoPermiso { get; set; }
+    }
+}
